f6106fa928348e99eeca5f77e671e4e935f5f34e
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2023-09-02 16:35:35 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -5,210 +5,50 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
   subscriptions.push(
     vscode.commands.registerCommand(extractMethodCommand, async () => {
       const editor: any = vscode.window.activeTextEditor;
-      if (editor.document.languageId === "python") {
-        if (editor) {
-          if (editor.selection.isEmpty) {
-            return vscode.window.showErrorMessage(
-              "Please Select Text before Extracting a Method",
-            );
-          } else if (
-            editor.selection.start.line === editor.selection.end.line
-          ) {
-            const { selection } = editor;
-            const { document } = editor;
-            const text = document.getText(selection);
-            const position = selection.start;
-            const edit = new vscode.WorkspaceEdit();
-            const methodName = await vscode.window.showInputBox({
-              prompt: "Enter Method Name",
-            });
-            if (methodName) {
-              const newText = `def ${methodName}():\n\t${text}`;
-              edit.delete(document.uri, selection);
-              edit.insert(document.uri, position, newText);
-              await vscode.workspace.applyEdit(edit);
-            } else {
-              return vscode.window.showErrorMessage("Method Name is Required");
-            }
-          } else if (
-            editor.selection.start.line !== editor.selection.end.line
-          ) {
-            const { selection } = editor;
-            const { document } = editor;
-            let text = document.getText(selection);
-            const position = selection.start;
-            const edit = new vscode.WorkspaceEdit();
-            const methodName = await vscode.window.showInputBox({
-              prompt: "Enter Method Name",
-            });
-            if (methodName) {
-              if (selection.start.line !== selection.end.line) {
-                if (selection.start.character === 0) {
-                  text = text.replace(/\n/g, "\n\t");
-                  text = `def ${methodName}():\n\t${text}`;
-                  edit.delete(document.uri, selection);
-                  edit.insert(document.uri, position, text);
-                  await vscode.workspace.applyEdit(edit);
-                } else {
-                  text = text.replace(/^\s+/, "");
-                  const newText = `def ${methodName}():\n\t${text}`;
-                  edit.delete(document.uri, selection);
-                  edit.insert(document.uri, position, newText);
-                  await vscode.workspace.applyEdit(edit);
-                }
-              } else {
-                const newText = `def ${methodName}():\n\t${text}`;
-                edit.delete(document.uri, selection);
-                edit.insert(document.uri, position, newText);
-                await vscode.workspace.applyEdit(edit);
-              }
-            } else {
-              return vscode.window.showErrorMessage("Method Name is Required");
-            }
-          }
-        } else {
-          return vscode.window.showErrorMessage(
-            "Open an Active Editor before Extracting a Method",
-          );
-        }
-        return undefined;
-      } else if (
-        editor.document.languageId === "typescript" ||
-        editor.document.languageId === "javascript"
-      ) {
-        if (editor) {
-          if (editor.selection.isEmpty) {
-            return vscode.window.showErrorMessage(
-              "Please Select Text before Extracting a Method",
-            );
-          }
-          if (editor.selection.start.line === editor.selection.end.line) {
-            const { selection } = editor;
-            const { document } = editor;
-            const text = document.getText(selection);
-            const position = selection.start;
-            const edit = new vscode.WorkspaceEdit();
-            const methodName = await vscode.window.showInputBox({
-              prompt: "Enter Method Name",
-            });
-            if (methodName) {
-              const newText = `function ${methodName}() {\n\t${text}\n}`;
-              edit.delete(document.uri, selection);
-              edit.insert(document.uri, position, newText);
-              await vscode.workspace.applyEdit(edit);
-            } else {
-              return vscode.window.showErrorMessage("Method Name is Required");
-            }
-          } else if (
-            editor.selection.start.line !== editor.selection.end.line
-          ) {
-            const { selection } = editor;
-            const { document } = editor;
-            let text = document.getText(selection);
-            const position = selection.start;
-            const edit = new vscode.WorkspaceEdit();
-            const methodName = await vscode.window.showInputBox({
-              prompt: "Enter Method Name",
-            });
-            if (methodName) {
-              if (selection.start.line !== selection.end.line) {
-                if (selection.start.character === 0) {
-                  text = text.replace(/\n/g, "\n\t");
-                  text = `function ${methodName}() {\n\t${text}\n}`;
-                  edit.delete(document.uri, selection);
-                  edit.insert(document.uri, position, text);
-                  await vscode.workspace.applyEdit(edit);
-                } else {
-                  text = text.replace(/^\s+/, "");
-                  const newText = `function ${methodName}() {\n\t${text}\n}`;
-                  edit.delete(document.uri, selection);
-                  edit.insert(document.uri, position, newText);
-                  await vscode.workspace.applyEdit(edit);
-                }
-              } else {
-                const newText = `function ${methodName}() {\n\t${text}\n}`;
-                edit.delete(document.uri, selection);
-                edit.insert(document.uri, position, newText);
-                await vscode.workspace.applyEdit(edit);
-              }
-            } else {
-              return vscode.window.showErrorMessage("Method Name is Required");
-            }
-          }
-        } else {
-          return vscode.window.showErrorMessage(
-            "Open an Active Editor before Extracting a Method",
-          );
-        }
-        return undefined;
-      } else if (editor.document.languageId === "c") {
-        // indent the code to be extracted
-        if (editor) {
-          if (editor.selection.isEmpty) {
-            return vscode.window.showErrorMessage(
-              "Please Select Text before Extracting a Method",
-            );
-          }
-          if (editor.selection.start.line === editor.selection.end.line) {
-            const { selection } = editor;
-            const { document } = editor;
-            const text = document.getText(selection);
-            const position = selection.start;
-            const edit = new vscode.WorkspaceEdit();
-            const methodName = await vscode.window.showInputBox({
-              prompt: "Enter Method Name",
-            });
-            if (methodName) {
-              const newText = `void ${methodName}()\n{\n\t${text}\n}`;
-              edit.delete(document.uri, selection);
-              edit.insert(document.uri, position, newText);
-              await vscode.workspace.applyEdit(edit);
-            } else {
-              return vscode.window.showErrorMessage("Method Name is Required");
-            }
-          } else if (
-            editor.selection.start.line !== editor.selection.end.line
-          ) {
-            const { selection } = editor;
-            const { document } = editor;
-            let text = document.getText(selection);
-            const position = selection.start;
-            const edit = new vscode.WorkspaceEdit();
-            const methodName = await vscode.window.showInputBox({
-              prompt: "Enter Method Name",
-            });
-            if (methodName) {
-              if (selection.start.line !== selection.end.line) {
-                if (selection.start.character === 0) {
-                  text = text.replace(/\n/g, "\n\t");
-                  text = `void ${methodName}()\n{\n\t${text}\n}`;
-                  edit.delete(document.uri, selection);
-                  edit.insert(document.uri, position, text);
-                  await vscode.workspace.applyEdit(edit);
-                } else {
-                  text = text.replace(/^\s+/, "");
-                  const newText = `void ${methodName}()\n{\n\t${text}\n}`;
-                  edit.delete(document.uri, selection);
-                  edit.insert(document.uri, position, newText);
-                  await vscode.workspace.applyEdit(edit);
-                }
-              } else {
-                const newText = `void ${methodName}()\n{\n\t${text}\n}`;
-                edit.delete(document.uri, selection);
-                edit.insert(document.uri, position, newText);
-                await vscode.workspace.applyEdit(edit);
-              }
-            } else {
-              return vscode.window.showErrorMessage("Method Name is Required");
-            }
-          }
-        } else {
-          return vscode.window.showErrorMessage(
-            "Open an Active Editor before Extracting a Method",
-          );
-        }
-        return undefined;
+      
+      if (!editor) {
+        return vscode.window.showErrorMessage("Open an Active Editor before Extracting a Method");
       }
+      
+      if (editor.selection.isEmpty) {
+        return vscode.window.showErrorMessage("Please Select Text before Extracting a Method");
+      }
+      
+      const { selection } = editor;
+      const { document } = editor;
+      const text = document.getText(selection);
+      const position = selection.start;
+      const edit = new vscode.WorkspaceEdit();
+      const methodName = await vscode.window.showInputBox({ prompt: "Enter Method Name" });
+      if (!methodName) {
+        return vscode.window.showErrorMessage("Method Name is Required");
+      }
+
+      // Helper function to replace text and insert into editor
+      const applyEdit = (newText: string) => {
+        edit.delete(document.uri, selection);
+        edit.insert(document.uri, position, newText);
+        return vscode.workspace.applyEdit(edit);
+      };
+
+      if (editor.document.languageId === "python") {
+        let newText = `def ${methodName}():\n\t${text}`;
+        await applyEdit(newText);
+      } 
+      else if (editor.document.languageId === "typescript" || editor.document.languageId === "javascript") {
+        let newText = `function ${methodName}() {\n\t${text}\n}`;
+        await applyEdit(newText);
+      } 
+      else if (editor.document.languageId === "c") {
+        let newText = `void ${methodName}()\n{\n\t${text}\n}`;
+        await applyEdit(newText);
+      } 
+      else if (editor.document.languageId === "csharp") {
+        // Added C# support
+        let newText = `public void ${methodName}()\n{\n\t${text}\n}`;
+        await applyEdit(newText);
+      }
+      // Add more languageId conditions as necessary
     }),
   );
 }
